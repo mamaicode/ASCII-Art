3c6e30a2e4c03b27a75b02137ca67dac7a1f9755
scirin <irakokakola@gmail.com>
2023-01-10 20:54:44 +0000

    Implement character cell averaging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use image::imageops::FilterType;
 use image::{DynamicImage, ImageFormat, GenericImageView};
 use std::fs::File;
 use std::io::{BufReader, Write};
@@ -15,26 +14,45 @@ fn main() {
     // Create an ASCII art representation of the image
     let ascii_art = image_to_ascii(&grayscale_image);
 
-    // Write the ASCII art to a file
+    // // Write the ASCII art to a file
     let mut output_file = File::create("output.txt").unwrap();
-    write!(output_file, "{}", ascii_art).unwrap();
+    write!(output_file, "{}", ascii_art).unwrap();   
 }
 
 fn image_to_ascii(image: &DynamicImage) -> String {
-
-    let new_image = image.resize( 500, 30, FilterType::Triangle);
-
-
-    let (width, height) = new_image.dimensions();
+    let (width, height) = image.dimensions();
+    let block_width = 10;
+    let block_height = 10;
     let mut ascii_art = String::new();
 
-    // Iterate over the pixels in the image
-    for y in 0..height {
-        for x in 0..width {
-            let pixel = new_image.get_pixel(x, y);
-            let brightness = (0.299 * f64::from(pixel[0])
-                + 0.587 * f64::from(pixel[1])
-                + 0.114 * f64::from(pixel[2])) as u8;
+    // Iterate over the blocks of pixels in the image
+    for y_block in 0..(height/block_height) {
+        for x_block in 0..(width/block_width) {
+            let mut red_sum = 0;
+            let mut green_sum = 0;
+            let mut blue_sum = 0;
+            let mut total_pixels = 0;
+
+            // Sum the color values of all pixels in the block
+            for y in (y_block*block_height)..((y_block+1)*block_height) {
+                for x in (x_block*block_width)..((x_block+1)*block_width) {
+                    let pixel = image.get_pixel(x, y);
+                    red_sum += pixel[0] as u32;
+                    green_sum += pixel[1] as u32;
+                    blue_sum += pixel[2] as u32;
+                    total_pixels += 1;
+                }
+            }
+
+            // Calculate the average color of the block
+            let avg_red = red_sum / total_pixels;
+            let avg_green = green_sum / total_pixels;
+            let avg_blue = blue_sum / total_pixels;
+
+            // Use the average color to map the block to an ASCII character
+            let brightness = (0.299 * f64::from(avg_red)
+                + 0.587 * f64::from(avg_green)
+                + 0.114 * f64::from(avg_blue)) as u8;
 
             let ascii_char = match brightness {
                 0..=50 => '#',
@@ -43,13 +61,12 @@ fn image_to_ascii(image: &DynamicImage) -> String {
                 151..=200 => '*',
                 201..=255 => ' ',
             };
-
             ascii_art.push(ascii_char);
         }
         ascii_art.push('\n');
     }
-
     ascii_art
 }
+
 
 
